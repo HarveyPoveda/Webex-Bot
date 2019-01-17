5e3a17abab4cdb9dd00f8a4020b84bbf9a2fd35d
HarveyPoveda <41435411+HarveyPoveda@users.noreply.github.com>
2019-01-17 15:04:06 -0500

    Add files via upload

--- WebexBot.py
+++ WebexBot.py
@@ -7,70 +7,106 @@ import requests
 import json
 import smartsheet
 from datetime import datetime
+import pprint
 import os
 
 
 #Access token can be generated using the CISCO BOT account and Bot Email also can find in there
-botEmail = "hpovedaf-local-bot@webex.bot"#bot's email address
-accessToken = os.environ['TOKEN'] #Bot's access token
-host = "https://api.ciscospark.com/v1/"#end point provided by the CISCO Spark to communicate between their services
+
+botEmail = "hpovedaf-local-bot@webex.bot"
+accessToken = os.environ['TOKEN'] 
+host = "https://api.ciscospark.com/v1"
 headers = {"Authorization": "Bearer %s" % accessToken,"Content-Type": "application/json"}
-room_id =  os.environ['room_id'] ## Room id where bot is added@app.route('/webhook', methods=['POST'])
 authetication_sheet=os.environ['autheticationsheet']
 
 
+
 @app.route('/', methods=['POST'])
-def get_tasks():
-    messageId = request.json.get('data').get('id')
-    messageDetails = requests.get(host+"messages/"+messageId, headers=headers)
-    replyForMessages(messageDetails)
+def getMessage():
+    message_json= request.json
+    #pprint.pprint(json.dumps(message))
+    messageId=message_json["data"]["id"]
+    room_id=message_json["data"]["roomId"]
+    message = requests.get(host + "/messages/" + messageId, headers=headers)
+    Answer(message,room_id)
     return ""
 
-#A function to send the message by particular email of the receiver
-def sendMessage(message,  toPersonEmail):
-    cisco="""
+
+#
+def sendMessage(message, room_id):
+    cisco = """
 .:|:.:|:. CISCO"""
-    payload = {"roomId": room_id, "text": message+"\n\n"+cisco}
-    response = requests.post("https://api.ciscospark.com/v1/messages/", data=json.dumps(payload),  headers=headers)
-    return response.status_code
-    
-#A function to get the reply and generate the response of from the bot's side
-## Add Bot logic for complicated bots 
-def replyForMessages(response):
+    payload = {"roomId": room_id, "text": message + "\n\n" + cisco}
+    requests.post(host+"/messages/", data=json.dumps(payload), headers=headers)
 
-    responseMessage = response.json().get('text')
-    toPersonEmail = response.json().get('personEmail')
-    print (responseMessage)
-    if toPersonEmail != botEmail:
-        if 'hola' in responseMessage:
-            messageString = """Hola, En que te puedo ayudar?
-1. Total de requerimientos atendidos
-2. Total de requerimientos de planeación"""
-            sendMessage(messageString,  toPersonEmail)
-        elif "1".lower() in responseMessage.lower():
+
+#
+def Answer(message_request, room_id):
+    global bandera
+    message_json = json.loads(message_request.text)
+    #pprint.pprint(json.dumps(message_json))
+    message=message_json["text"]
+    WhoisEmail = message_json["personEmail"]
+    if WhoisEmail != botEmail:
+        if bandera==0:
+            whois = requests.get(host + "/people?email=" + WhoisEmail, headers=headers)
+            WhoisName_json = json.loads(whois.text)
+            print(WhoisName_json)
+            WhoisName = WhoisName_json["items"][0]["firstName"]
+            if 'hola' in message.lower():
+                ans = "Hola "+WhoisName+"!\n" + menu()
+                sendMessage(ans, room_id)
+            elif "1" in message.lower():
+                ss_client = smartsheet.Smartsheet(authetication_sheet)
+                sheet_id = 7812041256789892
+                columnId = 8202839215368068
+                row_id = 7005051021485956
+                response = ss_client.Cells.get_cell_history(sheet_id, row_id, columnId, include_all=True)
+                # print(json.loads(str(response)))
+                messageString = "El total de requerimientos atendidos es de: " + json.loads(str(response))["data"][0][
+                    'displayValue'] + "\nal día de hoy: " + datetime.now().strftime('%A %d %B %Y')
+
+                # messageString = "it is " + datetime.now().strftime('%A %d %B %Y') + " today."
+                sendMessage(messageString, room_id)
+            elif "3" in message.lower():
+                ss_client = smartsheet.Smartsheet(authetication_sheet)
+                #row_id = 7005051021485956
+                #response = ss_client.Cells.get_cell_history(sheet_id, row_id, columnId, include_all=True)
+                # print(json.loads(str(response)))
+                messageString = "Ingrese el requerimiento a buscar"
+                bandera=3
+
+                # messageString = "it is " + datetime.now().strftime('%A %d %B %Y') + " today."
+                sendMessage(messageString, room_id)
+            else:
+                ans="Lo siento, no entiendo tu petición\n"+menu()
+                sendMessage(ans, room_id)
+        elif bandera==3:
+            bandera=0
             ss_client = smartsheet.Smartsheet(authetication_sheet)
-            """
-            sheet_id = 9005334300780420
-            columnId= 4255539602450308
-            row_id=4600688007243652
-            """
-            sheet_id = 7812041256789892
-            columnId= 8202839215368068
-            row_id=7005051021485956
-            response=ss_client.Cells.get_cell_history(sheet_id,row_id,columnId,include_all=True)
-            #print(json.loads(str(response)))
-            messageString = "El total de requerimientos atendidos es de: "+json.loads(str(response))["data"][0]['displayValue']+"\nal día de hoy: " + datetime.now().strftime('%A %d %B %Y')
+            sheet_id = 5067660233860996
+            response = ss_client.Search.search(message)
+            row_id=json.loads(str(response))["results"][0]["objectId"]
+            #columnId_requerimiento = 8796249076852612 # id de requerimiento
+            columnId_Asunto = 4292649449482116
+            columnId_Estado = 4714861914548100
+            response = ss_client.Cells.get_cell_history(sheet_id, row_id, columnId_Asunto, include_all=True)
+            messageString = "asunto: " + json.loads(str(response))["data"][0]['displayValue'] + "\n"
+            response = ss_client.Cells.get_cell_history(sheet_id, row_id, columnId_Estado, include_all=True)
+            messageString+="Estado: " + json.loads(str(response))["data"][0]['displayValue']+ "\n"
+            messageString+="\nal día de hoy: " +datetime.now().strftime('%A %d %B %Y')
+            sendMessage(messageString, room_id)
+            #print(result)
 
 
-            #messageString = "it is " + datetime.now().strftime('%A %d %B %Y') + " today."
-            sendMessage(messageString,  toPersonEmail)
-        else:
-            messageString = """Lo siento, no entiendo la pregunta, pero te puedo ayudar con:
-            
-1. Total de requerimientos atendidos
-2. Total de requerimientos de planeación"""
-            sendMessage(messageString, toPersonEmail)
+def menu():
+    messageString = """¿En que te puedo ayudar?
+1. Total de requerimientos atendidos 
+2. Total de requerimientos de planeación
+3. Buscar Caso"""
+
+    return messageString
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=int("5000"))
 
